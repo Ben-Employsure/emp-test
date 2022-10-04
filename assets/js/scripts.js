e96211a6719caeea1d1450a8bb711706721738c9
Ben-Employsure <113003446+Ben-Employsure@users.noreply.github.com>
2022-10-04 13:20:27 +1100

    Updated Lazy Loading

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,3 +28,17 @@ searchGo.addEventListener("click", function(){
     window.location.href = 'https://employsure.com.au/guides/' + searchCategory + '/';
 });
 
+// Lazy Loading
+  if ('loading' in HTMLImageElement.prototype) {
+      const images = document.querySelectorAll("img.lazyload");
+      images.forEach(img => {
+          img.src = img.dataset.src;
+      });
+  } else {
+      // Dynamically import the LazySizes library
+    let script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+    document.body.appendChild(script);
+  };
